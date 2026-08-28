0269075aa8c9735fe11ba788f75d97e38494de21
chinzozo <chinzoobatbold1@gmail.com>
2026-08-28 14:16:14 +0800

    2nd

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,27 +1,29 @@
 import "dotenv/config";
-import pg from "pg"; // <-- Заавал pg багцаас pool-ээ оруулж ирэх ёстой
+import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const connectionString = process.env.NEXT_PUBLIC_DATABASE_URL;
+const connectionString =
+  process.env.DATABASE_URL || process.env.NEXT_PUBLIC_DATABASE_URL;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL is not defined in .env file");
 }
 
-// PrismaPg адаптерт заавал pg.Pool-ийг дамжуулж өгөх ёстой
+// NeonDB requires SSL connection even during local development
 const pool = new pg.Pool({
   connectionString,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: { rejectUnauthorized: false },
 });
+
 const adapter = new PrismaPg(pool);
 
-// Global хувьсагч ашиглан хөгжүүлэлтийн явцад хэт олон холболт (Pool connection) үүсэхээс сэргийлнэ
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
